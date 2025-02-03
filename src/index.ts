4473821ef3c67b115b74c0703afbc91ed0a8e9ae
Shravan Kumar Karnati <shravank@truveta.com>
2025-02-02 18:28:47 -0800

    export DUMMY

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Graph, { Vertex, Edge } from '@/interface/graph';
 import { LayoutOptions } from '@/interface/definition';
 import { BaryCentricOptions } from '@/algos/barycentric';
 import { layout } from '@/algos/sugiyama';
+import { DUMMY } from '@/interface/constant';
 
-export { Graph, Vertex, Edge, LayoutOptions, BaryCentricOptions };
+export { Graph, Vertex, Edge, LayoutOptions, BaryCentricOptions, DUMMY };
 export default layout;
